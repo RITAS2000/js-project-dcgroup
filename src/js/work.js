019fb351a330281206c9539ef951dd809b7ea0e2
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-04-04 12:20:18 +0300

    test7

--- src/js/work.js
+++ src/js/work.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as basicLightbox from 'basiclightbox';
+import sprite from '../img/sprite.svg';
 
 const form = document.querySelector('.work-form');
 const input = document.querySelector('.work-input');
@@ -84,7 +85,7 @@ function getModalContent(type) {
       <div class="work-modal-sucssec">
         <button type="button" class="work-close-btn">
           <svg width="22" height="22">
-            <use href="../../img/sprite.svg#icon-close"></use>
+            <use href='${sprite}#icon-close'></use>
           </svg>
         </button>
         <h3 class="work-modal-title">Thank you for your interest in cooperation!</h3>
@@ -96,7 +97,7 @@ function getModalContent(type) {
       <div class="work-modal-sucssec">
         <button type="button" class="work-close-btn">
           <svg width="22" height="22">
-            <use href="../../img/sprite.svg#icon-close"></use>
+            <use href='${sprite}#icon-close'></use>
           </svg>
         </button>
         <h3 class="work-modal-title"><span class="work-modal-span-erorr">Oops!</span> Something went wrong.</h3>
