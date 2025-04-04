5eb11c861ccd1f671b9c9bc07e38d3232a8b4aa8
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-04-04 23:54:23 +0300

    test

--- src/js/work.js
+++ src/js/work.js
@@ -1,7 +1,6 @@
-
 import axios from 'axios';
 import * as basicLightbox from 'basiclightbox';
-import sprite from '../img/sprite.svg';
+import sprite from '../public/sprite2.svg';
 
 const form = document.querySelector('.work-form');
 const input = document.querySelector('.work-input');
@@ -107,4 +106,3 @@ function getModalContent(type) {
     `;
   }
 }
-
