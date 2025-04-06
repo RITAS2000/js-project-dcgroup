fe753466906c76d80fc722abd2abda6216a1285d
zeib01 <zeib01@users.noreply.github.com>
2025-04-06 18:06:22 +0200

    css with my style

--- src/js/aboutme.js
+++ src/js/aboutme.js
@@ -1,34 +1,57 @@
 import Accordion from 'accordion-js';
-import "accordion-js/dist/accordion.min.css";
-// import Swiper from 'swiper';
-// import 'swiper/css';
-// import 'swiper/css/navigation';
+import 'accordion-js/dist/accordion.min.css';
 
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-// Initialisiere accordion-js
-const accordionItems = document.querySelectorAll('.accordion-item');
-const accordionContent = document.querySelectorAll('.accordion-content');
+const aboutMeAccordion = new Accordion('.accordion-containerX', {
+  duration: 400,
+  elementClass: 'wrap-item',
+  triggerClass: 'icon-wrapper',
+  panelClass: 'about-cart',
+});
+aboutMeAccordion.open(0);
 
+function initAboutSwiper() {
+  const aboutSwiper = new Swiper('.about-sw', {
+    modules: [Navigation, Pagination], // Додаємо Pagination модуль
+    slidesPerView: 'auto',
+    spaceBetween: 20, // Додамо відстань між слайдами
 
-accordionItems.forEach(item =>
-    item.addEventListener("click", () => {
-        const activeContent = document.querySelector('#' + item.dataset.tab);
+    // Навігаційні стрілки
+    navigation: {
+      nextEl: '.about-swip-arrow-next',
+      prevEl: '.about-swip-arrow-prev',
+    },
 
-        if (activeContent.classList.contains("active")) {
-            activeContent.classList.remove("active");
-        } else {
-            accordionContent.forEach(element => {
-                element.classList.remove("active");
-                element.style.maxHeight = '0';
-            });
+    // Пагінація (крапки)
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true, // Зробити пагінацію клікабельною
+    },
 
-            accordionItems.forEach(element => element.classList.remove("active"));
+    breakpoints: {
+      // Адаптивні брейкпоінти для різної ширини екрану
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 30,
+      },
+    },
+  });
+}
 
-            item.classList.add('active');
-            activeContent.classList.add('active');
-            activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
-       }
-
-    })
-
-);
+// Викликаємо функцію ініціалізації Swiper після завантаження DOM
+document.addEventListener('DOMContentLoaded', initAboutSwiper);
